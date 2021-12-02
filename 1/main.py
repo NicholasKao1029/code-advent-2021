e1142b711bf8247de28db787a08617ea40edd046
Nick Kao <kobenkao@gmail.com>
2021-12-01 22:03:19 -0800

    day 1

--- 1/main.py
+++ 1/main.py
@@ -3,15 +3,33 @@ f = open(file_name, "r")
 
 txt_inputs = f.readlines()
 
-ans = 0
 num_inputs = list(map(lambda x: int(x), txt_inputs))
 
-prev = num_inputs[0]
 
-for num in num_inputs[1:]:
-    if num > prev: 
-        ans += 1
-    prev = num
+def sliding_3(lst):
+    length = len(lst)
+    ans = []
+    for i in range(length-2):
+        sum = 0
+        for j in range(3):
+            sum += lst[i+j]
+        ans.append(sum)
+    return ans
 
-print(ans)
-print(len(txt_inputs))
+
+sliding_3_window = sliding_3(num_inputs)
+
+def count_increasing(lst):
+    ans = 0
+    prev = lst[0]
+    for num in lst[1:]:
+        if num > prev: 
+            ans += 1
+        prev = num
+    return ans
+
+print('pt.1')
+print(count_increasing(num_inputs))
+print()
+print('pt.2')
+print(count_increasing(sliding_3_window))
