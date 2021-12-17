be7877a2cec1a69c9f977e8339d30d693e60b197
Nick Kao <kobenkao@gmail.com>
2021-12-17 07:07:21 -0800

    8 complete so hard

--- 8/main.py
+++ 8/main.py
@@ -70,6 +70,7 @@ total = calc_num_output_digits_unique_length(displays)
 print('pt1')
 print(total)
 print()
+print()
 
 
 # amount_of_section_map = {
@@ -89,66 +90,145 @@ def calculate_0(seven, one):
     print(seven, one)
     return (set(seven) - set(one)).pop()
 
-def calculate_6(len_5, zero, one, a):
-    sets_len_5 = [set(jumble) for jumble in len_5]
-
-    def reduce_func(something, next):
-        return something.intersection(next)
-    
-    common_intersection = reduce(reduce_func, sets_len_5)
-    
-    three = None
-    two_five = []
-
-    for set_obj in sets_len_5:
-        difference = set_obj - common_intersection
-        if difference == set(one):
-            three = set_obj
-        else:
-            two_five.append(set_obj)
-
-    some_diff = (two_five[0] - common_intersection) 
-    some_diff = some_diff - set(one)
-    eb_set = some_diff - (two_five[1] - common_intersection)
-
-    sides = eb_set.union(one)
-    ag_set = set(zero) - sides
-
-    return (ag_set - a).pop()
-
 # takes in an input line from a display
-def calculate_wiring(input):
+# will produce number to corresponding jumble
+def calculate_number(input):
+    input = input.split()
     n = len(input)
     assert(n == 10)
 
-    digit_to_string = [0] * n
+    num_to_jumble = [0] * n
 
     length_6 = []
     length_5 = []
 
-    wire_config = [0] * 7
     for jumble in input:
         jumble_len = len(jumble)
         if jumble_len == 7:
-            digit_to_string[8] = jumble
+            num_to_jumble[8] = jumble
         elif jumble_len == 3:
-            digit_to_string[7] = jumble
+            num_to_jumble[7] = jumble
         elif jumble_len == 4:
-            digit_to_string[4] = jumble
+            num_to_jumble[4] = jumble
         elif jumble_len == 2:
-            digit_to_string[1] = jumble
+            num_to_jumble[1] = jumble
         elif jumble_len == 5:
             length_5.append(jumble)
         elif jumble_len == 6:
             length_6.append(jumble)
-        
-    wire_config[0] = calculate_0(digit_to_string[7], digit_to_string[1])
-    wire_config[6] = calculate_6(length_5, digit_to_string[0], digit_to_string[1], wire_config[0])
 
-    return wire_config
+    two, three, five = handle_len_5(length_5, num_to_jumble[1], num_to_jumble[4])
+    num_to_jumble[2] = two
+    num_to_jumble[3] = three 
+    num_to_jumble[5] = five
+
+    zero, six, nine = handle_len_6(length_6, num_to_jumble[1], num_to_jumble[5])
+
+    num_to_jumble[0] = zero
+    num_to_jumble[6] = six 
+    num_to_jumble[9] = nine
+
+    return num_to_jumble
+
+# takes in list of jumbles with length 5
+def handle_len_5(jumbles, one, four):
+    two = None
+    three = None
+    five = None
+
+    set_len_5 = {j:set(j) for j in jumbles }
+
+    def reduce_func(something, next):
+        return something.intersection(next)
+    
+    common_intersection = reduce(reduce_func, set_len_5.values()) # should only contain a d g
+
+    assert(len(common_intersection) == 3)
+    leftovers = {s:(set_len_5[s] - common_intersection) for s in set_len_5 }
+
+    # intersect with one 
+    # if length left is 
+        # 2 it's three
+        # 1 it's two 
+        # 0 it's five
+
+    three_or_five = []
+
+    for l in leftovers:
+        leftover_set = leftovers[l]
+        intersect = leftover_set.intersection(one)
+        if len(intersect) == 1:
+            three_or_five.append(l)
+        if len(intersect) == 2:
+            three = l
+
+    four_set = set(four)
+    four_set = four_set - set(one)
+    four_set = four_set - (four_set.intersection(common_intersection))
+
+    assert(len(four_set) == 1)
+
+    for x in three_or_five:
+        sett = leftovers[x]
+        diff = sett - four_set
+        if len(diff) == 1:
+            five = x
+        else:
+            assert(len(diff) == 2)
+            two = x
+
+    return two, three, five
+
+def handle_len_6(jumbles, one, five):
+    zero = None
+    six = None
+    nine = None
+
+    five_set = set(five)
+    one_set = set(one)
+
+    set_len_6 = {j:set(j) for j in jumbles }
+
+    for s in set_len_6:
+        sett = set_len_6[s]
+        diff = sett - five_set
+
+        if len(diff) == 2:
+            zero = s
+        else:
+            assert(len(diff) == 1)
+            something = one_set - diff
+            if len(something) == 1:
+                nine = s
+            elif len(something) == 2:
+                six = s
 
 
+    return zero, six, nine
 
-print(calculate_wiring("fbead dcabe bcega gfbecd ecd dgac cd bedcag agebcfd fcagbe".split()))
 
+print(calculate_number(displays[0].first))
+
+
+def part2():
+    ans = 0
+    for display in displays:
+        input = display.first
+        code = display.output.split()
+        code = [set(s) for s in code]
+
+        translation = calculate_number(input)
+        translation = [set(x) for x in translation]
+
+        output = "" 
+        for c in code:
+            num = translation.index(c)
+            output += str(num)
+        ans += int(output)
+    return ans
+
+
+print('pt2')
+ans = part2()
+print(ans)
 
